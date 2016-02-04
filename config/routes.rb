26d2aeefa7357c6f980bbbeca6e75ef9fe5db590
master-marius <mastermicco@gmail.com>
2016-02-04 03:21:50 +0000

    Rails 5 Chatter setup

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Serve websocket cable requests in-process
+  mount ActionCable.server => '/cable'
+  resources :messages, only: [:index]
+  resources :sessions, only: [:new, :create]
+
+  root 'session#new'
+end
